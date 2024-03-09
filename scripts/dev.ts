b7848b50846c10f5cead68cb17e326126f935859
Chooks22 <49726759+Chooks22@users.noreply.github.com>
2024-03-09 22:07:02 +0800

    ‼️ BREAKING: update to tailwind 4 alpha

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -3,7 +3,7 @@ import app from '../src/index.js'
 
 const PORT = Number(process.env.PORT ?? '3000')
 
-await $`bun x tailwind -o public/styles.css`
+await $`bun x tailwindcss -i src/styles.css -o public/styles.css`
 
 app.listen(PORT, c => {
   console.info('Server started on %s 🚀', c.url)
